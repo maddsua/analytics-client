50e7b5a9539b4dc05e2a588003d663897780a0fd
maddsua <maddsuadev@proton.me>
2023-11-18 00:00:00 +0200

    Update analytics.ts

--- lib/analytics.ts
+++ lib/analytics.ts
@@ -1,62 +1,75 @@
-
-type GtagJSType = (command: 'js' | 'config' | 'event', eventName: any, eventParams?: object) => void;
-interface LocalWindow extends Window {
+interface WindowWithAnalytics extends Window {
 	dataLayer?: Array<object>;
-	gtag?: GtagJSType;
+	gtag?: (command: 'js' | 'config' | 'event', eventName: any, eventParams?: object) => void;
 };
 
-declare let window: LocalWindow;
+declare let window: WindowWithAnalytics;
 
-export class Analytics {
+interface LoaderInit {
+	tagid: string;
+	type?: 'gtag' | 'gtm';
+	trackClicks?: boolean;
+};
 
-	constructor(tagID: string) {
+export const loadGoogleAnalytics = (props: LoaderInit) => {
 
-		if (window.gtag) throw new Error('GA is already started');
+	const headScript = document.createElement('script');
+	headScript.src = `https://www.googletagmanager.com/gtag/js?id=${props.tagid}`;
+	headScript.async = true;
 
-		const headScript = document.createElement('script');
-		headScript.src = `https://www.googletagmanager.com/gtag/js?id=${tagID}`;
-		headScript.async = true;
-		document.head.appendChild(headScript);
-	
-		window.dataLayer = window.dataLayer || [];
-	
+	document.head.appendChild(headScript);
+
+	window.dataLayer = window.dataLayer || [];
+
+	if (props?.type === 'gtm') {
+		window.dataLayer.push({
+			'gtm.start': new Date().getTime(),
+			'event': 'gtm.js'
+		});
+	}
+	else {
+		
 		window.gtag = function () {
 			window.dataLayer!.push(arguments);
 		};
-	
+
 		window.gtag('js', new Date());
-		window.gtag('config', tagID);
+		window.gtag('config', props.tagid);
+	}
+
+	if (props?.trackClicks) {
 
 		document.querySelectorAll<HTMLLIElement>('[data-track]').forEach(elem => {
 
-			const eventID = elem.getAttribute('data-track-click');
+			const eventID = elem.getAttribute('data-track');
 			if (!eventID?.length) {
 				console.log('Element', elem, 'has empty event id [data-track]');
 				return;
 			}
 	
 			const clickHandler = () => {
-				this.pushEvent(eventID);
+				pushEvent(eventID);
 				elem.removeEventListener('click', clickHandler);
 			};
 	
 			elem.addEventListener('click', clickHandler);
 		});
-
 	}
+};
 
-	pushEvent(eventID: string, params?: Record<string, string>) {
+export const pushEvent = (eventID: string, props?: object) => {
 
-		if (!eventID.length) {
-			console.warn('eventID must not be empty');
-			return;
-		}
-
-		//	set event id to uppercase
+	//	send eventID first character to upper case
+	if (eventID[0].toUpperCase() !== eventID[0]) {
 		eventID = eventID[0].toUpperCase() + eventID.slice(1);
-
-		console.log('Reporting analytics event:', eventID);
-		(window.gtag as GtagJSType)('event', eventID, params);
 	}
 
+	console.log('Reporting analytics event:', eventID);
+
+	if (!window.dataLayer) {
+		console.warn('dataLayer is not available (yet?)');
+		window.dataLayer = window.dataLayer || [];
+	}
+
+	window.dataLayer.push(Object.assign({ 'event': eventID }, props || {}))
 };
